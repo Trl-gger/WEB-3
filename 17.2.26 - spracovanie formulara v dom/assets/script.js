580662c3d5d9a0dc94bdc4bb4a0bf808626f67a0
Trl-gger <simonhorvath556@gmail.com>
2026-03-17 08:04:27 +0100

    updated

--- 17.2.26 - spracovanie formulara v dom/assets/script.js
+++ 17.2.26 - spracovanie formulara v dom/assets/script.js
@@ -1,21 +1,25 @@
-const form = document.getElementById("newsletterForm");
+const form = document.getElementById('newsletterForm');
+// v premennej je celý Element formlára
 
-form.addEventListener("submit", handleSubmit);
+form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event){
-    event.preventDefault();
 
-    const emailInput = document.getElementById("email");
-    const email = emailInput.value;
+   event.preventDefault(); // Zastaví obnovenie stránky
 
-    const menoInput = document.getElementById("meno");
-    const meno = menoInput.value;
+  const menoInput = document.getElementById('meno');
+  const meno = menoInput.value;
+  
+  const emailInput = document.getElementById('email');
+  const email = emailInput.value;
 
-    const vsetkyCheckboxy = document.querySelectorAll('input[type="checkbox"]');
+  const vsetkyCheckboxy = document.querySelectorAll('input[type="checkbox"]');
 
-    const poleCheckboxov = Array.from(vsetkyCheckboxy);
+  const poleCheckboxov = Array.from(vsetkyCheckboxy); //prevod nodelisu (získali sme ho querySelectorAll) na pole (aby sme ho mohli filrovať)
 
-    const zaskrtnutie = poleCheckboxov.map(checkbox => checkbox.value)
+  const zaskrtnute = poleCheckboxov.filter(checkbox => checkbox.checked) //vytiahn s poľa checkboxov iba tie, ktoré sú zaškrnuté 
 
-    console.log(poleCheckboxov);
-}
+    const vybraneTexy = zaskrtnute.map(checkbox => checkbox.value);
+    console.log('Vybrané témy:', vybraneTexy);
+};
+
--- 3.3.2026 - polia, slovniky/script.js
+++ 3.3.2026 - polia, slovniky/script.js
@@ -3,12 +3,13 @@ mena.splice(1,2);
 mena.push("Jeremy", "George");
 
 cisla = [5,4,8,9,3,2];
-/**for (let i = 0, i < mena.lenght; i++){
+/**for (let i = 0, i < mena.length; i++){
     console.log(mena[i]);
 } */
 
+sucet = 0;
+
 for (let i=0; i < cisla.length; i++){
-    sucet = 0;
     sucet = sucet+ cisla[i];
     console.log ('pole:' + cisla[i]);
     console.log('sucet:' + sucet);
